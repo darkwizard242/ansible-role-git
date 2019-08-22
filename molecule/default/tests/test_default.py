961df32093de5274a10ce6c12723858570e3275e
Ali Muhammad <am900820@gmail.com>
2019-08-22 16:50:15 -0400

    revamped the pytest for git, added test sequence

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,6 +6,21 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_git(host):
-    gitpackage = host.package("git")
-    assert gitpackage.is_installed
+GIT_BINARY_PATH = '/usr/bin/git'
+PACKAGE = 'git'
+
+
+def test_git_package_installed(host):
+    host.package("PACKAGE").is_installed
+
+
+def test_git_binary_exists(host):
+    host.file('GIT_BINARY_PATH').exists
+
+
+def test_git_binary_file(host):
+    host.file('GIT_BINARY_PATH').is_file
+
+
+def test_git_binary_whereis(host):
+    host.check_output('whereis git') == GIT_BINARY_PATH
