3fe45cc06bde5e86fd80e6ad468efd15936b8e0e
darkwizard242 <am900820@gmail.com>
2020-11-25 20:54:10 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,17 +6,33 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+PACKAGE = 'git'
+PACKAGE_BINARY = '/usr/bin/git'
+
+
 def test_git_package_installed(host):
-    assert host.package("git").is_installed
+    """
+    Tests if git package is in installed state.
+    """
+    assert host.package(PACKAGE).is_installed
 
 
 def test_git_binary_exists(host):
-    assert host.file('/usr/bin/git').exists
+    """
+    Tests if git binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
 def test_git_binary_file(host):
-    assert host.file('/usr/bin/git').is_file
+    """
+    Tests if git binary is a file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
 def test_git_binary_which(host):
-    assert host.check_output('which git') == '/usr/bin/git'
+    """
+    Tests the output to confirm git's binary location.
+    """
+    assert host.check_output('which git') == PACKAGE_BINARY
